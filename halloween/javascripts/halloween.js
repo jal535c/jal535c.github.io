e6508cc6c67fc31f300dee62fd2e809ae0c1067f
Juan Luis <jal535c@gmail.com>
2019-10-31 19:24:15 +0100

    segunda version

---
+++ halloween/javascripts/halloween.js
@@ -0,0 +1,14 @@
+/* declaracion de las funciones */
+
+function mostrar(elemento) {
+    document.getElementById(elemento).style.visibility="visible";
+}
+function ocultar(elemento) {
+    document.getElementById(elemento).style.visibility="hidden";
+}
+function sonar(sonido) {
+    document.getElementById(sonido).play();
+}
+function pausar(sonido) {
+    document.getElementById(sonido).pause();
+}
