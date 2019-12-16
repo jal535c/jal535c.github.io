c8b531ff60b00642de75bd9ec80dc65d5a604b09
Juan Luis <jal535c@gmail.com>
2019-12-16 11:30:19 +0100

    examen

---
+++ examen/scripts.js
@@ -0,0 +1,9 @@
+/* Funciones javascript para la SideNav */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
